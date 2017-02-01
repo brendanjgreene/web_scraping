fe54c13d22966ce44e9bdc51019c2aed8760e79a
Brendan Greene <brendanjgreene@gmail.com>
2017-02-01 20:43:37 +0000

    Regular expressions - ignoring cases

--- regex_test.py
+++ regex_test.py
@@ -1,9 +1,12 @@
 import re
 
-# Using * asterisk - multiple occurrences of the character preceding it
+print "# Using * asterisk - multiple occurrences of the character preceding it"
 print re.findall("ab*c", "ac")  # ['ac']
 print re.findall("ab*c", "abcd")  # ['abc']
 print re.findall("ab*c", "acc")  # ['ac']
 print re.findall("ab*c", "abcac")  # ['abc', 'ac']
 print re.findall("ab*c", "abdc")  # []
 print re.findall("ab*c", "ABC")  # [] case sensitive
+
+print '# Using re.IGNORECASE'
+print re.findall("ab*c", "ABC", re.IGNORECASE)  # ['ABC']
